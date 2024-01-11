c30d9eeef265e890d050cad0d744a74f10d8fa36
Alexey Fedechkin <alex@centralhardware.me>
2024-01-11 20:42:22 +0100

    fix rdata is None

--- src/collector.py
+++ src/collector.py
@@ -55,15 +55,13 @@ if __name__ == '__main__':
             except KeyError:
                 cached = False
 
-            try:
-                t = DNSRecord.parse(base64.b64decode(j['Answer']))
-                rdata = t.a.rdata
-                rcode = t.header.rcode
-            except AttributeError:
-                rdata = '0.0.0.0'
-                rcode = 0
+            t = DNSRecord.parse(base64.b64decode(j['Answer']))
 
-            data = [[date_time, j['QH'], j['QT'], j['QC'], j['CP'], upstream,j['Answer'], j['IP'], isFiltered, j['Elapsed'], cached, rdata, rcode]]
+            rdata = t.a.rdata
+            if rdata is None:
+                rdata = '0.0.0.0'
+
+            data = [[date_time, j['QH'], j['QT'], j['QC'], j['CP'], upstream,j['Answer'], j['IP'], isFiltered, j['Elapsed'], cached, rdata, t.header.rcode]]
             print(data)
             clickhouse.insert(table, data,
                               ['date_time', 'QH', 'QT', 'QC', 'CP', 'Upstream', 'Answer', 'IP', 'IsFiltered','Elapsed', 'Cached', 'rdata', 'rcode'])
