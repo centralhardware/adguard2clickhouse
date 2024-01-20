e0d3001b33f85571fc20a11174097a7e0df1c8b6
Alexey Fedechkin <alex@centralhardware.me>
2024-01-20 09:33:32 +0100

    refactor

--- src/collector.py
+++ src/collector.py
@@ -1,69 +1,90 @@
 import base64
+import json
 import logging
 import os
 import traceback
 
 import clickhouse_connect
-import json5
 import tailer
-from dateutil import parser
 from dnslib import DNSRecord
 from netaddr import valid_ipv4, valid_ipv6
 
-user = os.getenv("DB_USER")
-password = os.getenv("DB_PASSWORD")
-host = os.getenv("DB_HOST")
-database = os.getenv("DB_DATABASE")
-clickhouse = clickhouse_connect.get_client(host=host, database=database, port=8123, username=user, password=password,
-                                           settings={'async_insert': '1', 'wait_for_async_insert': '0'})
 
-def process_line(line):
-    j = json5.loads(line)
+class DNSQuery:
+    def __init__(self, dns_query):
+        self.log = json.loads(dns_query)
+        self.date_time = self.log['T']
+        self.query_address = self.log['QH']
+        self.query_type = self.log['QT']
+        self.query_class = self.log['QC']
+        self.client_proto = self.log['CP']
+        self.upstream_addr = self.log.get('Upstream', '')
+        self.ip_address = self.log['IP']
+        self.is_filtered = self.log.get('Result', {}).get('IsFiltered', False)
+        self.elapsed = self.log['Elapsed']
+        self.is_cached = self.log.get('Cached', False)
+        self.r_code, self.r_datas, self.r_datas6, self.c_names = self.parse_dns_record()
 
-    date_time = parser.isoparse(j['T'])
-    try:
-        isFiltered = j['Result']['IsFiltered']
-    except KeyError:
-        isFiltered = False
-
-    try:
-        upstream = j['Upstream']
-    except KeyError:
-        upstream = ''
-
-    try:
-        cached = j['Cached']
-    except KeyError:
-        cached = False
-
-    t = DNSRecord.parse(base64.b64decode(j['Answer']))
-    rdatas = []
-    rdatas6 = []
-    cnames = []
-    for pr in t.rr:
-        if pr.rdata is not None and valid_ipv4(str(pr.rdata)):
-            rdatas.append(str(pr.rdata))
-        elif pr.rdata is not None and valid_ipv6(str(pr.rdata)):
-            rdatas6.append(str(pr.rdata))
-        elif pr.rdata is not None:
-            cnames.append(str(pr.rdata))
-
-    data = [
-        [date_time, j['QH'], j['QT'], j['QC'], j['CP'], upstream, j['IP'], isFiltered, j['Elapsed'],
-         cached, t.header.rcode, rdatas, rdatas6, cnames]]
-    clickhouse.insert('log2', data,
-                      ['date_time', 'QH', 'QT', 'QC', 'CP', 'Upstream', 'IP', 'IsFiltered', 'Elapsed',
-                       'Cached', 'rcode', 'rdatas', 'rdatas6', 'cnames'])
+    def parse_dns_record(self):
+        t = DNSRecord.parse(base64.b64decode(self.log['Answer']))
+        rdatas = []
+        rdatas6 = []
+        cnames = []
+        for pr in t.rr:
+            if pr.rdata is not None and valid_ipv4(str(pr.rdata)):
+                rdatas.append(str(pr.rdata))
+            elif pr.rdata is not None and valid_ipv6(str(pr.rdata)):
+                rdatas6.append(str(pr.rdata))
+            elif pr.rdata is not None:
+                cnames.append(str(pr.rdata))
+        return t.header.rcode, rdatas, rdatas6, cnames
 
 
-if __name__ == '__main__':
+def main():
     open("/code/querylog.log", 'w').close()
     logging.basicConfig(level=logging.INFO, format='%(message)s')
     logging.info('start application')
+    config = get_config()
+
+    clickhouse = clickhouse_connect.get_client(host=config['host'], database=config['database'], port=8123,
+                                               username=config['username'], password=config['password'],
+                                               settings={'async_insert': '1', 'wait_for_async_insert': '0'})
 
     for line in tailer.follow(open("/code/querylog.log")):
         try:
-            process_line(line)
+            dns_query = DNSQuery(line)
+            data = [[
+                dns_query.date_time,
+                dns_query.query_address,
+                dns_query.query_type,
+                dns_query.query_class,
+                dns_query.client_proto,
+                dns_query.upstream_addr,
+                dns_query.ip_address,
+                dns_query.is_filtered,
+                dns_query.elapsed,
+                dns_query.is_cached,
+                dns_query.r_code,
+                dns_query.r_datas,
+                dns_query.r_datas6,
+                dns_query.c_names
+            ]]
+            clickhouse.insert('log2', data,
+                              ['date_time', 'QH', 'QT', 'QC', 'CP', 'Upstream', 'IP', 'IsFiltered', 'Elapsed',
+                               'Cached', 'rcode', 'rdatas', 'rdatas6', 'cnames'])
         except Exception:
             traceback.print_exc()
             print(line)
+
+
+def get_config():
+    # Check if the config file exists and read data from it if it is
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    host = os.getenv("DB_HOST")
+    database = os.getenv("DB_DATABASE")
+    return {"user": user, "password": password, "host": host, "database": database}
+
+
+if __name__ == "__main__":
+    main()
